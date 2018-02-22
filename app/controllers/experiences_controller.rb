59abc3d5d500502ef544cb73808f88c71e907304
Ashley Amin <ashley.starks@gmail.com>
2018-02-22 15:27:18 -0500

    created ability to add new experience

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -3,18 +3,33 @@ class ExperiencesController < ApplicationController
     @experiences = Experience.all
   end
 
-  def new
-    @experience = Experience.new
-    # @product = Product.find(params[:product_id])
+  def show
+    @experience = Experience.find(params[:id])
   end
 
+  def new
+    @experience = Experience.new
+  end
+
+  def update
+    @experience = Experience.find(params[:id])
+    if @experience.update_attributes(experience_params)
+      redirect_to experiences_path
+    else
+      render :edit
+    end
+  end
+  
   def create
     @experience = Experience.new(experience_params)
+    if @experience.save
+      redirect_to experience_path(@experience)
+    end
   end
 
   private
 
   def experience_params
-    params.require(:experience).permit(:name, :description, :tip, :product_id, :title)
+    params.require(:experience).permit(:product_id, :title, :description, :tip, :name)
   end
 end
