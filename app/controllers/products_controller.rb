8e7d0302d8fb5fb4896d37b1aaf4b33f5712373f
Ashley Amin <ashley.starks@gmail.com>
2018-02-23 10:20:06 -0500

    completed all CRUD functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,37 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    end
+  end
+
+  def destroy
+      @product = Product.find(params[:id])
+      @product.destroy
+      redirect_to categories_path
+    end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:category_id, :title, :description, :website, :contact)
+  end
+
+
 end
